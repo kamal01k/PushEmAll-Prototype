b2dca4c63393bbaba688a581ed69985c5d71149f
kamal01k <kamal01k02@gmail.com>
2022-08-24 19:01:19 +0530

    stick push animation
    stuck push animation.

--- PushEmAll/Assets/Scripts/Player/PlayerController.cs
+++ PushEmAll/Assets/Scripts/Player/PlayerController.cs
@@ -8,6 +8,9 @@ namespace Assignment.Player
         [SerializeField] private GameObject _stickObject;
         [SerializeField] private float _speed = 10;
         [SerializeField] private float _rotationSpeed = 5;
+        [SerializeField] private float pushTime = 4;
+        [SerializeField] private float pushDistance = 4;
+        private float stickDefaultDistance = 0;
 
         public bool ControllerActive {get; set;}
         private Transform m_transform;
@@ -23,6 +26,8 @@ namespace Assignment.Player
             m_transform = transform;
             m_rb = GetComponent<Rigidbody>();
             ControllerActive = false;
+
+            stickDefaultDistance = _stickObject.transform.localPosition.z;
         }
 
         // Update is called once per frame
@@ -32,20 +37,22 @@ namespace Assignment.Player
             {
                 return;
             }
-            
+
             _moveInput = Input.GetAxis("Vertical");
             _rotaionInput = Input.GetAxis("Horizontal");
 
             if(Input.GetKeyUp(KeyCode.Space))
             {
-                Debug.Log($"Space up");
                 AnimateStickPush();
             }
         }
 
         private void AnimateStickPush()
         {
-            _stickObject.transform.DOMoveZ(3,2).SetEase(Ease.InOutBounce);
+            _stickObject.transform.DOLocalMoveZ(pushDistance, pushTime/2).SetEase(Ease.Linear)
+            .OnComplete(()=>{
+                _stickObject.transform.DOLocalMoveZ(stickDefaultDistance, pushTime/2).SetEase(Ease.Linear);
+            });
         }
 
         private void FixedUpdate()
